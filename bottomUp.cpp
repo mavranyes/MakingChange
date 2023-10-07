9855fa07cff062b5dba5a6e8869a4cf370d4bd02
Ethan-43 <113789098+Ethan-43@users.noreply.github.com>
2023-10-07 16:26:54 -0400

    Update bottomUp.cpp

--- bottomUp.cpp
+++ bottomUp.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
-#include <iostream>
-#include <fstream>
 #include <string>
 #include <vector>
+#include "RBTree.cpp"
 
 using namespace std;
 
 vector <string> bottomUp(vector <int> denoms, vector <int> problems) {
 	vector <string> solutions;
+	for (int i = 0; i < problems.size(); i++) {
+		
+
+	}
 	return solutions;
 }
