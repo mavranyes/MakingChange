e774628c7b4d0764ac29d511ddeec541f4008310
Ethan-43 <113789098+Ethan-43@users.noreply.github.com>
2023-10-06 21:09:48 -0400

    Parse test file

--- topDown.cpp
+++ topDown.cpp
@@ -1,4 +1,30 @@
 #include <iostream>
 
-using namespace std
+using namespace std;
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	// Reading Input
+	string line = "";
+	vector <string> file;
+		// From File
+	
+	ifstream input("test.txt", ios::in);
+	while (getline(input, line)) {
+		file.push_back(line);
+		}
+		
+		// From stdin
+
+
+
+
+	return 0;
+}
 
