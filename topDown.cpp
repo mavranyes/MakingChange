2b8d79768fc109af1ce6fc8510bde1c59366f6ad
Ethan-43 <113789098+Ethan-43@users.noreply.github.com>
2023-10-06 23:56:15 -0400

    Parsing input
    I commented out several blocks that might be needed for testing/debugging later

--- topDown.cpp
+++ topDown.cpp
@@ -12,8 +12,8 @@ int main() {
 	// Reading Input
 	string line = "";
 	vector <int> file;
-		// From File
-
+	
+	// From File
 	ifstream input("test.txt", ios::in);
 	while (getline(input, line)) {
 		file.push_back(stoi(line, nullptr, 10));
@@ -23,21 +23,38 @@ int main() {
 		cout << file[i] << "\n";
 	}
 	*/
-		// From stdin
+	
+	// From stdin
 
-
+	//Get n from the first line of the input
 	int n = file[0];
-	cout << "N: " << n << "\n";
+	//cout << "N: " << n << "\n";		//Print n
 
+	//Populate denoms vector with the next n lines of input
 	vector <int> denoms;
-	for (int i = 1; i < n + 1; i++) {
+	for (int i = 1; i < (n + 1); i++) {
 		denoms.push_back(file[i]);
 	}
-	
+	/*
 	for (int i = 0; i < denoms.size(); i++) {
 		cout << "DENOMS[" << i << "]: " << denoms[i] << "\n";
 	}
-	
+	*/
+
+	//Get k from the n+1 line of input
+	int k = file[n + 1];
+	//cout << "K: " << k << "\n";		//Print k
+
+	//Populate the problems vector with input lines from k to the end of input
+	vector <int> problems;
+	for (int i = (n + 2); i < (k + n + 2); i++) {
+		problems.push_back(file[i]);
+	}
+	/*
+	for (int i = 0; i < problems.size(); i++) {
+		cout << "PROBLEMS[" << i << "]: " << problems[i] << "\n";
+	}
+	*/
 
 	return 0;
 }
