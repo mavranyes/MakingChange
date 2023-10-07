b056738cc0ac12d74ccc5ba4e9317a007731af9e
Ethan-43 <113789098+Ethan-43@users.noreply.github.com>
2023-10-06 23:44:29 -0400

    Update topDown.cpp

--- topDown.cpp
+++ topDown.cpp
@@ -6,20 +6,38 @@
 
 using namespace std;
 
+
+
 int main() {
 	// Reading Input
 	string line = "";
-	vector <string> file;
-	// From File
+	vector <int> file;
+		// From File
 
 	ifstream input("test.txt", ios::in);
 	while (getline(input, line)) {
-		file.push_back(line);
+		file.push_back(stoi(line, nullptr, 10));
 	}
+	/*
 	for (int i = 0; i < file.size(); i++) {
 		cout << file[i] << "\n";
 	}
-	// From stdin
+	*/
+		// From stdin
+
+
+	int n = file[0];
+	cout << "N: " << n << "\n";
+
+	vector <int> denoms;
+	for (int i = 1; i < n + 1; i++) {
+		denoms.push_back(file[i]);
+	}
+	
+	for (int i = 0; i < denoms.size(); i++) {
+		cout << "DENOMS[" << i << "]: " << denoms[i] << "\n";
+	}
+	
 
 	return 0;
 }
