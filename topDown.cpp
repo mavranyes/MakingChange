62c63cd8cf90a1afe0dde6e2c6a46075a66e9fe3
Ethan-43 <113789098+Ethan-43@users.noreply.github.com>
2023-10-07 17:09:02 -0400

    Update topDown.cpp

--- topDown.cpp
+++ topDown.cpp
@@ -5,12 +5,8 @@
 
 using namespace std;
 
-vector <string> topDown(vector <int> denoms, vector <int> problems) {
-	vector <string> solutions;
-	for (int i = 0; i < problems.size(); i++) {
+string topDown(vector <int> denoms, int problem) {
+	string solution;
 
-
-
-	}
-	return solutions;
+	return solution;
 }
